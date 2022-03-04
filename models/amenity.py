2f07e98d74fd883c13d52f9113dcc7e9f46c3cec
alixvox <alixvox@gmail.com>
2022-03-03 20:54:09 -0800

    Adding Base inheritance to classes

--- models/amenity.py
+++ models/amenity.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
